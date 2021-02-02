7665327e06cc8b08b4561cdd10b4f39bc0639237
Jacob Fredriksson <jacobfredriksson@live.se>
2021-02-02 14:49:54 +0100

    second commit

---
+++ index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+const PORT = 8000;
+app.get("/example", (req, res) =>
+  res.send("Express + TypeScript Servera Wadap brothaaaaa ")
+);
+
+app.post("/", (req, res) => {});
+
+app.listen(PORT, () => {
+  console.log(`[server]: Server is running at https://localhost:${PORT}`);
+});
