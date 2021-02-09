0f8b850a658571dc2ee70d8b27201e70a9896c9a
Jacob Fredriksson <jacobfredriksson@live.se>
2021-02-09 14:38:42 +0100

    message

--- index.ts
+++ index.ts
@@ -6,7 +6,11 @@ app.get("/example", (req, res) =>
   res.send("Express + TypeScript Servera Wadap brothaaaasa ")
 );
 
-app.post("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.json({
+    message: "Yoyo",
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`[server]: Server is running at http://localhost:${PORT}`);
