dda82f50cc101133b8e0bb1fd3d77544c27511e2
AnassZaki <Thezaki.anass@gmail.com>
2021-02-09 15:04:00 +0100

    add get items endpoint

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express from "express";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+
 app.get("/example", (req, res) =>
   res.send("Express + TypeScript Servera Wadap brothaaaasa ")
 );
@@ -10,6 +11,14 @@ app.get("/", (req, res) => {
   res.json({
     message: "Yoyo",
   });
+});
+
+app.get("/items", (req, res) => {
+  res.json([
+    { id: 1, todo: "do homework" },
+    { id: 2, todo: "sleep" },
+    { id: 3, todo: "watch movie" },
+  ]);
 });
 
 app.listen(PORT, () => {
