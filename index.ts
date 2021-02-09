0b8c89f6ac8f469d167d36f3c77010bfb246d97f
Jacob Fredriksson <jacobfredriksson@live.se>
2021-02-09 14:44:38 +0100

    added script

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 app.get("/example", (req, res) =>
   res.send("Express + TypeScript Servera Wadap brothaaaasa ")
 );
